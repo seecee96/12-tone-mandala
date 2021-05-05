fd7905ce62123cf67bfcb59f776d36a7bdecca5a
corruptcatalyst <davidcampbell@alum.calarts.edu>
2021-05-04 18:45:53 -0700

    fix isSmall break
    not using fda approved vairables!!!

--- sketch.js
+++ sketch.js
@@ -5,15 +5,18 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
     isMobile = true;
 }
-var isSmall = false
  var heix = screen.height 
- 
+ var a = 0
+ var b = 0
 if (heix<800){
   isMobile =true
-  isSmall=true
-  
+  a= 80
+  b=150
 }
+console.log("mobile?",isMobile)
 console.log(heix)
+
+
 let slider
 let slider1
 let slider2
@@ -731,15 +734,8 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
- if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
+
   }
      sel1.option('Pick a Song')
 
@@ -749,8 +745,6 @@ if(isMobile==false){
         sel1.option('Great Gig',3);
       sel1.option('In A Sentimental Mood',19)
         sel1.option('Hey Jude',4);
-    sel1.option('Brahms',33);
-
 
 
     sel1.changed(songChange);
@@ -765,15 +759,8 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
- if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
+
   }
  
   sel1.option('Pick a Song')
@@ -802,15 +789,7 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
-       if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
 
   }
   
@@ -844,15 +823,7 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
-      if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
 
   }
   sel1.option('Pick a Song')
@@ -879,15 +850,7 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
-    if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
 
   }
     sel1.option('Pick a Song')
@@ -912,15 +875,8 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
- if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
+
   }  
   if(fileUpload==true){
       sel1.option('Pick a Song')
@@ -954,14 +910,7 @@ if(isMobile==false){
   slider.position(300, 223);
 }
   if(isMobile==true){
-    
-    if(isSmall==false){
-    slider.position(200, 222);
-    }
-    
-      if(isSmall==true){
-    slider.position(200, 142);
-    }
+    slider.position(200, 222-a);
   }
   slider.style('width', '500px');
   
@@ -971,13 +920,7 @@ if(isMobile==false){
   }
   if(isMobile==true){
     
-    if(isSmall=false){
-      slider1.position(200, 267);
-  }
-    
-     if(isSmall=true){
-      slider1.position(200, 187);
-  }
+      slider1.position(200, 267-a);
   }
   slider1.style('width', '500px');
   
@@ -987,15 +930,7 @@ if(isMobile==false){
   slider2.position(130, 502);
   }
   if(isMobile==true){
-     if(isSmall=false){
-    
-    slider2.position(110, 484);
-     }
-    
-       if(isSmall=true){
-    
-    slider2.position(110, 334);
-     }
+    slider2.position(110, 484-b);
     
   }
   slider2.style('width', '500px');
@@ -1007,19 +942,7 @@ if(isMobile==false){
   sel.position(213, 331); 
   }
    if(isMobile==true){
-        if(isSmall=false){
-    
-  
-  sel.position(133, 303);
-        }
-     
-         if(isSmall=true){
-    
-  
-  sel.position(133, 213);
-        }
-     
-     
+  sel.position(133, 303-a);
   }
   
   sel.option('Music',0);
@@ -1030,21 +953,10 @@ if(isMobile==false){
   
   sel2 = createSelect()
   if(isMobile==false){
-    
     sel2.position(339, 427);
   }
   if(isMobile==true){
-    
-     if(isSmall=false){
-        sel2.position(219, 440);
-
-     }
-     if(isSmall=true){
-        sel2.position(219, 288);
-
-     }
-    
-    
+    sel2.position(219, 440-b);
   }
      sel2.option('Most Clear',5);
 
@@ -1064,16 +976,7 @@ if(isMobile==false){
   }
   
   if(isMobile==true){
-    
-     if(isSmall=false){
-      sel1.position(313, 440);
-       
-     }
-    
-    if(isSmall=true){
-      sel1.position(313, 288);
-       
-     }
+      sel1.position(313, 440-b);
 
   }
     sel1.option('Clair de Lune',0);
@@ -1082,7 +985,6 @@ if(isMobile==false){
         sel1.option('Great Gig',3);
       sel1.option('In A Sentimental Mood',19)
         sel1.option('Hey Jude',4);
-    sel1.option('Brahms',33);
 
 
 
@@ -1105,19 +1007,8 @@ if(isMobile==false){
      button2.position(588, 463);
   }
    if(isMobile==true){
-     
-          if(isSmall=false){
-
-  button1.position(517, 427);
-          button2.position(588, 443);
-          }
-     
-        if(isSmall=true){
-
-  button1.position(517, 288);
-          button2.position(628, 288);
-          }
-     
+  button1.position(517, 427-b);
+          button2.position(628, 427-b);
 
   }
   button1.mousePressed(changeBS);
@@ -1141,8 +1032,7 @@ if(isMobile==false){
   GConcept = new TonePoint(width * 0.773205, height * 0.226795,7)
   midiPlayer= new MidiPlayer()
   turntable = new Turntable()
-
-
+  
   //give Skeleton.js it's tone points
   skeleton = new Skeleton(CConcept, FConcept, BbConcept, EbConcept, AbConcept, DbConcept, GbConcept,
                            BConcept, EConcept, AConcept, DConcept, GConcept)
